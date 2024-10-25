abb13984dd63954bd76d20ad571bea586ecb8ca3
Patryk Okenczyc <Xomel@outlook.com>
2024-10-25 12:12:01 +0200

    Zadanie4

---
+++ Zadanie3/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string website = "https://www.google.com/search?q=pogoda";
+            string findSymbol = "°";
+            string start = ">";
+            string end = "<";
+            int temperatura = 0; 
+            List<string> miasta = new List<string>();
+            List<int> srTemperatura = new List<int>();
+            
+
+
+            while (true)
+            {
+
+                Console.WriteLine("Podaj miasto : ");
+                string city = Console.ReadLine().ToLower();
+                miasta.Add(city);
+                WebClient wc = new WebClient();
+                string dane = wc.DownloadString(website + city);
+                int tempPlace = dane.IndexOf(findSymbol);
+                int i = tempPlace;
+                int j = tempPlace;
+
+                while (dane.Substring(i, 1) != start)
+                {
+                    i--;
+                }
+                while (dane.Substring(j, 1) != end)
+                {
+                    j++;
+                }
+                temperatura = Convert.ToInt32(dane.Substring((i + 1), (tempPlace - i - 1))); 
+                string temp = dane.Substring((i + 1), (j - i - 1));
+                Console.WriteLine(temp);
+                srTemperatura.Add(temperatura);
+
+                double srednia = 0;
+                foreach (var s in srTemperatura)
+                    srednia += s;
+
+                srednia /= srTemperatura.Count;
+
+                string raport = string.Format("Średnia temperatura z lista ({0}) to {1} °C",
+                        string.Join(",", miasta), srednia);
+                Console.WriteLine(raport);
+
+            }
+        }
+    }
+}
---
+++ Zadanie4/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Podaj kraj zawodnika : ");
+            string kraj = Console.ReadLine().ToLower();
+
+            string website = "https://tomaszles.pl/wp-content/uploads/2019/06/zawodnicy.txt";
+            WebClient wc = new WebClient();
+            string dane = wc.DownloadString(website);
+
+            string[] separatorWierszy = {"\r\n"};
+            string[] wiersze = dane.Split(separatorWierszy, StringSplitOptions.RemoveEmptyEntries);
+
+            List<string> zawodnicy = new List<string>();
+
+            for (int i = 1; i < wiersze.Length; i++)
+            {
+                string[] komorki = wiersze[i].Split(';');
+                if (komorki[4].ToLower() == kraj)
+                    zawodnicy.Add(komorki[2] + " " + komorki[3]);
+            }
+
+            string sciezkaWynik = @"D:\dane\kraje";
+            if(!Directory.Exists(sciezkaWynik))
+                Directory.CreateDirectory(sciezkaWynik);
+
+
+            if(!File.Exists(sciezkaWynik + $@"\{kraj}.txt"))
+                File.WriteAllLines(sciezkaWynik + $@"\{kraj}.txt", zawodnicy);
+
+           
+
+        }
+    }
+}
